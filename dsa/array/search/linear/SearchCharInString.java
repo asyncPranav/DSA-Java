cb75ac391fa0fd341ad8415564f11824e5c57955
redxcyber <mrdark9628@gmail.com>
2025-08-19 14:18:35 +0530

    add file to linear search

--- dsa/array/search/linear/SearchCharInString.java
+++ dsa/array/search/linear/SearchCharInString.java
@@ -31,4 +31,17 @@ public class SearchCharInString {
         }
         return -1;
     }
+
+    // method-02
+    static boolean search2(String str, char target){
+        if (str.length() == 0) {
+            return false;
+        }
+        for (char ch : str.toCharArray()){
+            if (ch == target) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
